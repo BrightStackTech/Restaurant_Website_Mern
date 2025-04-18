f1579c2fcc7a164c8d61662bf530038f311d5b72
BrightStackDev <brightstack.work.01@gmail.com>
2025-04-19 04:33:24 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -9,8 +9,8 @@ const { OAuth2Client } = require('google-auth-library');
 const googleClient = new OAuth2Client(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_CALLBACK_URL
-);
+  `${process.env.BACKEND_URL}/api/auth/google/callback`
+); 
 
 // Generate JWT token
 const generateToken = (id) => {
